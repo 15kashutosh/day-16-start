ca9710d87a3ef78c7dbc131c3812750f25df357b
Ashutosh <15kashutosh@gmail.com>
2022-01-22 22:24:55 +0530

    pretty table usage

--- main.py
+++ main.py
@@ -1,24 +1,37 @@
-# object oriented programming
-import another_module
-
-print(another_module.another_variable)
-
-# import turtle
+# # object oriented programming
+# import another_module
 #
+# print(another_module.another_variable)
+# refer https://docs.python.org/3/library/turtle.html for turtle documentation
+# # import turtle
+# #
+# # # creating turtle object called timmy
+# # timmy = turtle.Turtle()
+#
+# from turtle import Turtle, Screen
 # # creating turtle object called timmy
-# timmy = turtle.Turtle()
+# timmy = Turtle()
+# print(timmy)
+# # changed the pen to turtle shape
+# timmy.shape("turtle")
+# # changed the turtle color to red
+# timmy.color("red")
+# timmy.forward(100)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+#
+# # Object Methods
+# my_screen.exitonclick()
 
-from turtle import Turtle, Screen
-# creating turtle object called timmy
-timmy = Turtle()
-print(timmy)
-# changed the pen to turtle shape
-timmy.shape("turtle")
-# changed the turtle color to red
-timmy.color("red")
+# refer https://pypi.org/project/prettytable/ for PrettyTable documentation
+from prettytable import PrettyTable
+table = PrettyTable()
+#print(table)
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+# aligning the table to the left
+table.align = "l"
+print(table)
 
-my_screen = Screen()
-print(my_screen.canvheight)
-
-# Object Methods
-my_screen.exitonclick()
+# object attributes
