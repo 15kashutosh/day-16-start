6d2fb43582e5a64173d4d41464ab331c9427c0f7
Ashutosh <15kashutosh@gmail.com>
2022-01-22 18:18:40 +0530

    created day-16 of 100 days of code

---
+++ main.py
@@ -0,0 +1,19 @@
+# object oriented programming
+import another_module
+
+print(another_module.another_variable)
+
+# import turtle
+#
+# # creating turtle object called timmy
+# timmy = turtle.Turtle()
+
+from turtle import Turtle, Screen
+# creating turtle object called timmy
+timmy = Turtle()
+print(timmy)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+
+# Object Methods
